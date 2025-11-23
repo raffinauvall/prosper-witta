a326f319634871a80d85023501e59716d0658bad
raffinauvall <raffinauvaltaqy@gmail.com>
2025-11-23 21:44:54 +0700

    feat: improve TypeScript typings for RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import "../styles/globals.css";
 import "../styles/font.css"
+import React from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className="font-maison">
