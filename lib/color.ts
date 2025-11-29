52ecb4ec9e233e186a8bf7096f4acbc320fe02a2
raffinauvall <raffinauvaltaqy@gmail.com>
2025-11-30 02:15:58 +0700

    feat: add color text utility

---
+++ lib/color.ts
@@ -0,0 +1,8 @@
+export const Colors = {
+  veterinary: "text-amber-600",
+  mining: "text-yellow-600",
+  cleaner: "text-sky-600",
+  metal: "text-slate-600",
+  homeCare: "text-emerald-600",
+  waterTreament: "text-blue-600"
+} as const;
