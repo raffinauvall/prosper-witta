6d3e632ec391d4040ea84f0499ec9eb82536b023
raffinauvall <raffinauvaltaqy@gmail.com>
2025-12-11 00:13:43 +0700

    chore: formatting document

--- src/components/home/AboutSection.tsx
+++ src/components/home/AboutSection.tsx
@@ -1,12 +1,12 @@
 export default function AboutSection() {
   return (
     <section id="about" className="bg-white py-24 px-6 md:px-16 text-white">
-      
+
       <div className="max-w-6xl mx-auto">
 
         {/* Heading */}
         <div className="grid md:grid-cols-2 gap-16 items-start mb-4">
-          
+
           <h2 className="text-3xl md:text-5xl font-bold leading-tight text-black">
             Supplying More Than Chemicals —
 
@@ -16,15 +16,14 @@ export default function AboutSection() {
           </h2>
 
           <p className="text-gray-700 text-base md:text-lg leading-relaxed">
-           PT Prosper Witta Sejahtera is a chemical trading company which
-
-established in 2024. We serve industrial applications such as : Home
-& Personal Care, Institutional & Industrial Cleaner, Veterinary,
-Mining, Water Treatment, Metal Working, and Oil & Gas.
+            PT Prosper Witta Sejahtera is a chemical trading company which
+            established in 2024. We serve industrial applications such as : Home
+            & Personal Care, Institutional & Industrial Cleaner, Veterinary,
+            Mining, Water Treatment, Metal Working.
           </p>
         </div>
 
-        
+
 
 
       </div>
