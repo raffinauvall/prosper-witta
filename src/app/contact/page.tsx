9d153c2c21af73cc880db41883f3d638873f4b6f
raffinauvall <raffinauvaltaqy@gmail.com>
2025-12-11 00:13:04 +0700

    refactor: changing url embed map

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -70,10 +70,11 @@ export default function ContactUs() {
             <div className="mt-12">
               <iframe
                 className="w-full h-64 rounded-2xl shadow-md border border-gray-200"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.764301609701!2d106.8232603!3d-6.165100!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5c5e0485b6b%3A0xf2b8a4f39c5dde3!2sMonas!5e0!3m2!1sen!2sid!4v1702149829382"
+                src="https://maps.google.com/maps?q=PT+Prosper+Witta+Sejahtera&t=&z=13&ie=UTF8&iwloc=&output=embed"
                 loading="lazy"
               ></iframe>
             </div>
+            
           </motion.div>
 
           {/* Right: Contact Form */}
