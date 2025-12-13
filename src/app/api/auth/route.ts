e57ca7c8ad362ce54dc36fc97d02c47b93b40bd0
raffinauvall <raffinauvaltaqy@gmail.com>
2025-12-14 00:31:03 +0700

    refactor: change key

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: Request) {
 
         const supabase = createClient(
             process.env.NEXT_PUBLIC_SUPABASE_URL!,
-            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+            process.env.SUPABASE_SERVICE_ROLE_KEY!
         );
 
         const { data: user } = await supabase
