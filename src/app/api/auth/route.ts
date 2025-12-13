da8322248e197e7f6521eedc3cc7e27ce091eaf3
raffinauvall <raffinauvaltaqy@gmail.com>
2025-12-14 00:39:27 +0700

    fix: fix typo key

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: Request) {
 
         const supabase = createClient(
             process.env.NEXT_PUBLIC_SUPABASE_URL!,
-            process.env.SUPABASE_SERVICE_KEY!
+            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
         );
 
         const { data: user } = await supabase
