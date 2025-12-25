ab186624ebf26ebb82cb236f62359200c23f9fcc
raffinauvall <raffinauvaltaqy@gmail.com>
2025-12-26 00:51:42 +0700

    fix: fix middleware cookie authentication

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,26 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const token = req.cookies.get('token')?.value;
-  if (req.nextUrl.pathname.startsWith('/admin') && !token) {
-    const loginUrl = new URL('/login', req.url);
+  const token = req.cookies.get("session_token")?.value;
+
+  const isAdminRoute = req.nextUrl.pathname.startsWith("/admin");
+  const isLoginPage = req.nextUrl.pathname === "/login";
+
+  if (isAdminRoute && !token) {
+    const loginUrl = new URL("/login", req.url);
     return NextResponse.redirect(loginUrl);
   }
+
+
+  if (isLoginPage && token) {
+    const adminUrl = new URL("/admin", req.url);
+    return NextResponse.redirect(adminUrl);
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/admin'],
+  matcher: ["/admin/:path*", "/login"],
 };
