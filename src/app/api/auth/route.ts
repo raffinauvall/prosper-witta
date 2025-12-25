54a431b57de8b593a3c697db1ce5c5f657b42eea
raffinauvall <raffinauvaltaqy@gmail.com>
2025-12-26 00:33:22 +0700

    fix: fixing direct url

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -34,7 +34,7 @@ export async function POST(req: Request) {
         res.cookies.set("session_token", token, {
             httpOnly: true,
             secure: true,
-            path: "/",
+            path: "/admin",
             maxAge: 60 * 60 * 24 * 7,
         });
 
