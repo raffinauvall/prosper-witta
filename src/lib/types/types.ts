3512e4855994d4487aaf22e31dbb0cecd902ac63
raffinauvall <raffinauvaltaqy@gmail.com>
2026-01-05 01:07:04 +0700

    feat: update types for DocumentAccessRequest and add AccessStatus handling

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -19,6 +19,7 @@ export type Product = {
   image: string;
   product_categories: ProductCategory[];
   display: boolean;
+  status: AccessStatus;
 };
 
 export type ProductDocument = {
@@ -33,9 +34,11 @@ export type ProductDocument = {
 
 export type DocumentAccessRequest = {
   id: string; // uuid
-  document_id: string;
+  product_id: string;
   device_token: string;
 
+  name: string;
+  email: string;
   company?: string;
   purpose?: string;
 
@@ -43,6 +46,7 @@ export type DocumentAccessRequest = {
   token: string; 
 
   created_at?: string;
+  decided_at?: string | null;
 };
 
 export type DocumentAccessToken = {
@@ -62,3 +66,23 @@ export type DocumentWithProduct = ProductDocument & {
   product: Pick<Product, "id" | "name">;
 };
 
+export type AccessStatus = "none" | "pending" | "approved" | "rejected";
+
+export type DocumentAccessStatus = {
+  msds: AccessStatus;
+  tds: AccessStatus;
+};
+
+
+export type ProductMsdsProps = {
+  productId: number;
+  status: AccessStatus;
+  onRequest: (type: "msds") => void;
+  onView: () => void;
+};
+export type ProductTdsProps = {
+  productId: number;
+  status: AccessStatus;
+  onRequest: (type: "tds") => void;
+  onView: () => void;
+};
