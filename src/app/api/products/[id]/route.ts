1af8369ea09b5a88ce15dedb029f82cb57889d93
raffinauvall <raffinauvaltaqy@gmail.com>
2026-01-07 00:59:41 +0700

    refactor: change permission for editting and deleting product

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -8,7 +8,7 @@ interface Params {
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
 );
 
 export const DELETE = async (
