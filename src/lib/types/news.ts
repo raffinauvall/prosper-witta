0227b47adb3e3eed3c143970aba602123d6ddc9e
raffinauvall <raffinauvaltaqy@gmail.com>
2026-01-20 01:34:35 +0700

    feat: adapting interface and type for News

--- src/lib/types/news.ts
+++ src/lib/types/news.ts
@@ -1,8 +1,30 @@
 export interface News {
   id: number;
   title: string;
+  slug?: string;
+  excerpt?: string;
+  content?: string;
+  thumbnail_url?: string;
+  is_published?: boolean;
+  published_at?: string;
+}
+
+export interface NewsForm {
+  title: string;
   slug: string;
   excerpt: string;
-  thumbnail_url: string | null;
+  content: string;
+  thumbnail_url: string;
+  is_published: boolean;
   published_at: string;
 }
+
+export type NewsFormData = {
+  title: string;              
+  slug?: string;
+  excerpt?: string;
+  content?: string;
+  thumbnail_url?: string;
+  is_published: boolean;     
+  published_at?: string;
+};
