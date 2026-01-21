66cd035ea2e74b5990e1232ba5add71c45b41f79
raffinauvall <raffinauvaltaqy@gmail.com>
2026-01-21 22:29:09 +0700

    faet: add helper for normalizing array

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,6 +1,11 @@
-function cleanPayload<T extends Record<string, any>>(obj: T) {
+export function cleanPayload<T extends Record<string, any>>(obj: T) {
   Object.keys(obj).forEach(
     (key) => obj[key] === undefined && delete obj[key]
   );
   return obj;
+}
+export function normalizeArray<T>(res: T[] | { data: T[] } | null | undefined): T[] {
+  if (Array.isArray(res)) return res;
+  if (res && Array.isArray((res as any).data)) return (res as any).data;
+  return [];
 }
