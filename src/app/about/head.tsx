aede9549d696d6900593e8f8ed13be8af5717861
raffinauvall <raffinauvaltaqy@gmail.com>
2026-01-21 22:31:17 +0700

    feat: add head title

---
+++ src/app/about/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>About | PT Contoh Perusahaan</title>
+      <meta
+        name="description"
+        content="Selamat datang di website PT Contoh Perusahaan."
+      />
+    </>
+  );
+}
