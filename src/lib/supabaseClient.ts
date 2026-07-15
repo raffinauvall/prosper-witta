4660c7a09534f885eb20d6fb3abc24c418de5e2a
raffinauvall <raffinauvaltaqy@gmail.com>
2026-07-15 13:18:02 +0700

    fix: lazy initialize supabase clients

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { lazySupabaseClient } from "./lazySupabaseClient";
 
-export const supabaseClient = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+export const supabaseClient = lazySupabaseClient(
+  "NEXT_PUBLIC_SUPABASE_URL",
+  "NEXT_PUBLIC_SUPABASE_ANON_KEY"
 );
